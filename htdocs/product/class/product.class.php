86cb3d0e8947f42bd495dd50f7b240c6670b73c0
atm-florian <florian.mortgat@atm-consulting.fr>
2024-04-24 17:11:18 +0200

    "guirlandes" de backport

--- htdocs/product/class/product.class.php
+++ htdocs/product/class/product.class.php
@@ -3408,7 +3408,7 @@ class Product extends CommonObject
 			while ($obj = $this->db->fetch_object($result)) {
 				/** DEB : BACKPORT STANDARD PR28879**/
 				if ($obj->role == 'toconsume' && empty($warehouseid)) {
-				/** FIN : BACKPORT STANDARD PR28879 **/
+					/** FIN : BACKPORT STANDARD PR28879 **/
 					$this->stats_mrptoconsume['customers'] += $obj->nb_customers;
 					$this->stats_mrptoconsume['nb'] += $obj->nb;
 					$this->stats_mrptoconsume['rows'] += $obj->nb_rows;
@@ -3416,7 +3416,7 @@ class Product extends CommonObject
 				}
 				/** DEB : BACKPORT STANDARD PR28879 **/
 				if ($obj->role == 'consumed' && empty($warehouseid)) {
-				/** FIN : BACKPORT STANDARD PR28879 **/
+					/** FIN : BACKPORT STANDARD PR28879 **/
 					//$this->stats_mrptoconsume['customers'] += $obj->nb_customers;
 					//$this->stats_mrptoconsume['nb'] += $obj->nb;
 					//$this->stats_mrptoconsume['rows'] += $obj->nb_rows;
@@ -5522,6 +5522,7 @@ class Product extends CommonObject
 			}
 		}
 
+		/* BEGIN BACKPORT DOLIBARR : DA024318 [PR](https://github.com/Dolibarr/dolibarr/pull/29396)  */
 		if (! preg_match('/novirtual/', $option)) {
 			// initialize $this->stock_warehouse for all warehouses, even those with no current physical stock because
 			// they may still have a non-zero virtual stock
@@ -5537,6 +5538,8 @@ class Product extends CommonObject
 				}
 			}
 		}
+		/* END BACKPORT DOLIBARR : DA024318 [PR](https://github.com/Dolibarr/dolibarr/pull/29396) */
+
 
 		$sql = "SELECT ps.rowid, ps.reel, ps.fk_entrepot";
 		$sql .= " FROM ".$this->db->prefix()."product_stock as ps";
